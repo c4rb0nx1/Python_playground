ed6b2c750548ffd6a1379261f1802acc78de7f4f
Niranchan <90444898+c4rb0nx1@users.noreply.github.com>
2023-01-29 14:42:45 +0530

    Update Z_star_pattern.py

--- Z_star_pattern.py
+++ Z_star_pattern.py
@@ -1,11 +1,11 @@
 # z pattern in python
 n=int(input(""))
 for i in range(0,n):
-    print("*",end="")
+    print("*",end=" ")
 print("")
 for j in range(1,n-1):
     for a in range(j,n-1):
-        print(" ",end ="")
+        print(" ",end =" ")
     print("*")
 for x in range(0,n):
-    print("*",end="")
+    print("*",end=" ")
